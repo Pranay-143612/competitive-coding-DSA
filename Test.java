dd1aeecca139e803bc3328ce4959feea6abf1906
pranay <pranayacharya2004@gmail.com>
2025-09-06 19:06:11 +0530

    segemnt tress

---
+++ Test.java
@@ -0,0 +1,16 @@
+class Test{
+    public static void main(String[] args) {
+        int x; //declaration
+        x=143; //assignment
+        int y=23; //initialization
+        System.out.println(x);
+        System.out.println(y);
+        //float f=3.14f; //common convention for float u must use f at last of value
+        double d=3.14; //f is not used and most used one
+        boolean val = true;
+        char e='A';
+        String name ="Pranay";
+        String value = name + e; //concatination
+        System.out.println(value+ " My x is "+x+" and y is "+y);
+    }
+}
