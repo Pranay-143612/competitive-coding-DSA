db3b9faa20e244cfe74b41e7f7305851962a3bc5
pranay <pranayacharya2004@gmail.com>
2025-10-14 20:58:18 +0530

    Completely built Binary Tree

--- trees_da/BST.java
+++ trees_da/BST.java
@@ -16,18 +16,18 @@ public class BST {
     static Scanner sc = new Scanner(System.in);
     static private Node root;
 
-    public int height(Node node) {
+    public static int height(Node node) {
         if(node==null) {
             return -1;
         }
         return node.height;
     }
 
-    public void insert(int value) {
-
+    public static void insert(int value) {
+        root = insert(value,root);
     }
 
-    public Node insert(int value,Node node) {
+    public static Node insert(int value,Node node) {
         if(node==null) {
             node = new Node(value);
             return node;
@@ -42,7 +42,37 @@ public class BST {
         return node;
     }
 
+    public static boolean balanced() {
+        return balanced(root);
+    }
+
+    public static boolean balanced(Node node) {
+        if(node==null) {
+            return true;
+        }
+        return Math.abs(height(node.left)-height(node.right))<=1 && balanced(node.left) && balanced(node.right);
+    }
+
+    public static void display() {
+        display(root, "Root Element: ");
+    }
+
+    public static void display(Node node, String msg) {
+        if(node==null) return;
+        System.out.println(msg+node.value);
+        display(node.left,"Left child of "+node.value+": ");
+        display(node.right, "Right child of "+node.value+": ");
+    }
+
     public static void main(String[] args) {
-        
+        System.out.println("Enter number of elements in the tree:");
+        int n = sc.nextInt();
+        System.out.println("Enter tree elements");
+
+        for(int i=0;i<n;i++) {
+            insert(sc.nextInt());
+        }
+        display();
+        System.out.println("Is the tree balanced? "+balanced());
     }
 }
